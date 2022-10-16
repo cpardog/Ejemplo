a20aafbb7e253bb34ddf413aeed2dc90252e272b
Charlie <55631934+cpardog@users.noreply.github.com>
2022-10-16 11:42:23 -0500

    Add files via upload
    Subidos por primera vez

---
+++ Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine(Now.ToString)
+        Console.ReadKey()
+        ' Este es mi repositorio actualizado al domingo
+        ' Que viba Visual basic - Gracias
+    End Sub
+End Module
